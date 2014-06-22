3a22f4a6697d0679a87f14aa44133371990a4c3d
Troy Forster <troy.forster@tbwa-toronto.com>
2014-06-22 17:56:29 -0400

    Resolved serial port issues; Improved README.md; Added project image.

--- modules/tweetie-pi.js
+++ modules/tweetie-pi.js
@@ -1,120 +1,115 @@
-﻿var path = require("path"), basePath = path.dirname(require.main.filename);
-var twitter = require("ntwitter");
-var serialPort = require("serialport");
+﻿var path = require("path"), basePath = path.dirname(require.main.filename)
+var twitter = require("ntwitter")
+var SerialPort = require("serialport").SerialPort
 
 var tweetiePi = function () {
-   var self = this;
-   this.twitter = {};
+   var self = this
+   this.twitter = {}
+   var serialPort
 
-   this.speak = function (phrase) {
-      if (self.speak.serialPort) {
-         self.speak.serialPort.write("S" + phrase + "\n");
-      }
-      console.log(phrase);
-   };
+   var speak = function (phrase) {
+      serialPort.write("S" + phrase + "\n", function (err, results) {
+         if (!err) {
+            // Do nothing
+         }
+         else {
+            console.error(err)
+         }
+      })
+   }
 
    this.init = function (options) {
-      self.options = options;
-      self.twitter = new twitter(self.options.twitterCredentials);
+      self.options = options
+      self.twitter = new twitter(self.options.twitterCredentials)
 
-      // open the serial port if we can
-      if (isPortAvailable(options.serialPort)) {
-         self.speak.serialPort = new serialPort.SerialPort(options.serialPort, {
-            baudrate: 9600
-         });
-      }
-      else {
-         console.error(options.serialPort + " was not found.");
-      }
+      serialPort = new SerialPort(options.serialPort, {
+         baudRate: options.baudRate
+      }, false)
+
+      serialPort.open(function () {
+         // Wake up the Emic 2 board
+         serialPort.write("\n", function (err, results) {
+            if (!err) {
+               console.log("Emic 2 is awake")
+               serialPort.write("N" + options.voice + "\n")                  
+               speak(options.startupPhrase)
+            }
+            else {
+               console.error(err)
+            }
+         })
+      })
 
       self.twitter.stream("user", { with: "following" }, function (stream) {
-         console.log("listening to the Twitter stream...");
+         console.log("listening to Twitter stream")
          stream.on("data", function (data) {
             if (data.friends) {
-               for (var i = 0, len = data.friends.length; i < len; i++) {
-                  //console.log(data.friends[i]);
-               }
+               for (var i = 0, len = data.friends.length; i < len; i++) {}
             }
             else {
                // Check for filtered followers
                for (var i = 0, len = self.options.twitterHandles.length; i < len; i++) {
                   if (data.user.name === self.options.twitterHandles[0] || self.options.twitterHandles[i] === "*") {
-                     console.log(data);
-                     var tweet = writtenToSpoken(data);
-                     self.speak(tweet.user + tweet.action + tweet.text);
+                     var phrase = writtenToSpoken(data)
+                     console.log(data.text, "\n", phrase, "\n\n")
+                     speak(phrase)
                   }
                }
             }
-         });
+         })
 
          stream.on("end", function (response) {
             // Handle a disconnection
-         });
+         })
 
          stream.on("destroy", function (response) {
             // Handle a "silent" disconnection from Twitter, no end/error event fired
-         });
-      });
+         })
+      })
    }
 
-   var isPortAvailable = function (port) {
-      var portExists = false;
-      serialPort.list(function (err, ports) {
-         ports.forEach(function (port) {
-            if (port.comName == port) {
-               portExists = true;
-            }
-            console.log(port.comName);
-            console.log(port.pnpId);
-            console.log(port.manufacturer);
-         });
-      });
-      return portExists;
-   }
-
+   // Parses a tweet and transforms to a more colloquial sentence
    var writtenToSpoken = function(tweetObject) {
-      var speek = {};
+      var text = tweetObject.text
 
-      // [username] said | retweeted | favourited [tweet text]
-      var action = " said ";
+      var action = " said "
 
       if (tweetObject.retweeted) {
-         action = " retweeted ";
+         action = " retweeted "
       }
 
       if (tweetObject.favorited) {
-         action = " favourited ";
+         action = " favourited "
       }
 
-      if (tweetObject.text.indexOf("RT ") > -1) {
-         action = " retweeted ";
-         tweetObject.text = tweetObject.text.replace(/RT/g, "");
+      if (text.indexOf("RT ") > -1) {
+         action = " retweeted "
+         text = text.replace(/RT/g, "")
       }
 
       // Replace certain symbols with words because the text-to-speech parser doesn't pronounce symbols properly
-      tweetObject.text = tweetObject.text.replace(/@/g, " at ");
-      tweetObject.text = tweetObject.text.replace(/#/g, " hash tag ");
-
+      text = text.replace(/@/g, " at ")
+      text = text.replace(/#/g, " hash tag ")
+      text = text.replace(/(http|ftp|https):\/\/([\w\-_]+(?:(?:\.[\w\-_]+)+))([\w\-\.,@?^=%&amp:/~\+#]*[\w\-\@?^=%&amp/~\+#])?/gi, " url ")
+      
       // Some tweets include a populated place object
-      var from = "";
+      var from = ""
       if (tweetObject.place) {
-         from = " from " + tweetObject.place;
+         from = " from " + tweetObject.place
       }
 
-      from = tweetObject.location;
-      //var s = tweetObject.user.name + action + tweetObject.text + from;     
-      var reply = "";
+      from = tweetObject.location
+      var reply = ""
+
       if (tweetObject.in_reply_to_screen_name) {
-         reply = tweetObject.in_reply_to_screen_name;
+         reply = tweetObject.in_reply_to_screen_name
       }
-      return {
-         "user": tweetObject.user.name,
-         "action": action,
-         "text": tweetObject.text,
-         "from": from,
-         "reply": reply
-      }
+
+      // Cleanup any extraneous whitespace
+      var text = (tweetObject.user.name + action + text).replace(/\s{2,}/g, ' ').trim()
+
+      return text
    }  
 }
 
-module.exports = new tweetiePi();
+module.exports = new tweetiePi()
